bed0af501dd46397f3e64631cf6fbe212ff546d5
Dantallope <dantebraccialini@gmail.com>
2022-12-22 21:10:19 -0700

    framework for routes

---
+++ controllers/pathRouter.js
@@ -0,0 +1,13 @@
+const {Router} = require("express");
+
+const pathRouter = new Router();
+
+pathRouter.get('/', (req, res) => {
+    res.render("home");
+});
+
+pathRouter.get('/login', (req, res) => {
+    res.render("login");
+});
+
+module.exports = pathRouter;
