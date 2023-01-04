3a0c46b6b741d8d6178d3de55a415e058216b480
Dantallope <dantebraccialini@gmail.com>
2023-01-04 14:16:13 -0700

    its alive

--- controllers/apis/users.js
+++ controllers/apis/users.js
@@ -1,4 +1,6 @@
-const { Router} = require('express');
+const { Router } = require('express');
+
+const User = require('../../models/User');
 
 const usersRouter = new Router();
 
@@ -6,6 +8,6 @@ usersRouter.post("/login", async(req,res) => {
     console.log(req.body);
 
     res.end();
-})
+});
 
 module.exports = usersRouter;
