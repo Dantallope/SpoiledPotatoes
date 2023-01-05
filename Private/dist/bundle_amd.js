2c14ad5263ef0814c22a144fa31605f4433a2c7b
Jerry Wan <craftersol728@gmail.com>
2023-01-04 18:49:12 -0800

    added constt anime error msgs and else option

--- Private/dist/bundle_amd.js
+++ Private/dist/bundle_amd.js
@@ -149,3 +149,30 @@ const anime_details = (config) => {
   const API$4 = {
     url: "https://api.spoiledpotato.net/v2/anime",
   };
+
+  const anime = (config = {}) => {
+    config.url = API$4.url;
+    if (config.client_id == undefined) {
+      throw new ReferenceError("Missing MAL require: client_id");
+    }
+    if (config.limit == undefined) {
+      config.limit = 100;
+    }
+    if (config.offset == undefined) {
+      config.offset = 0;
+    }
+    if (config.fields == undefined) {
+      config.fields =
+        "id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics";
+    }
+    if (config.sort == undefined) {
+      config.sort = "anime_score";
+    } return {
+        anime_list: anime_list(config),
+        anime_details: anime_details(config),
+        anime_ranking: anime_ranking(config),
+        seasonal_anime: seasonal_anime(config),
+        suggested_anime: suggested_anime(config),
+    };
+};
+
