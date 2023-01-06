6153aefdfffe0b3f9a4f364503c1e80247419d63
Jerry Wan <craftersol728@gmail.com>
2023-01-05 17:54:44 -0800

    ADDED ROLL up js file

---
+++ Private/rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import globals from "rollup-plugin-node-globals";
+import builtins from "rollup-plugin-node-builtins";
+
+export default {
+  input: "index.js",
+  external: ["cross-fetch"],
+  output: {
+    name: "my-anime-list",
+    format: "umd",
+    file: "dist/bundle.cjs",
+    globals: {
+      "cross-fetch": "fetch",
+    },
+  },
+  plugins: [resolve(), commonjs(), globals(), builtins()],
+}
