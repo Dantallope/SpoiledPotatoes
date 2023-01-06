9c569137da24f19a0d64a80bf870a2f8c0c1c226
Xavier <xaviervelasquez2002@gmail.com>
2023-01-05 18:55:49 -0800

    Fixed routing for the view pages

--- controllers/pathRouter.js
+++ controllers/pathRouter.js
@@ -3,15 +3,11 @@ const {Router} = require("express");
 const pathRouter = new Router();
 
 pathRouter.get('/', (req, res) => {
-    res.render("home");
+    res.render("landing");
 });
 
 pathRouter.get('/login', (req, res) => {
     res.render("login");
-});
-
-pathRouter.get('/home', (req, res) => {
-    res.render("landing");
 });
 
 pathRouter.get('/users', (req, res) => {
