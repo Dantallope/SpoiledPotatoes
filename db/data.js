b2e56e15cf713b3cb702c01b4197444882d3c225
Jerry Wan <craftersol728@gmail.com>
2023-01-08 22:45:51 -0800

    added module export

--- db/data.js
+++ db/data.js
@@ -1,128 +1,17 @@
-<?xml version="1.0" encoding="UTF-8"?>
+module.exports = {
+    id: 1,
+    ageRestricted: false,
+    type: 'Anime',
+    episodeCount: 13,
+    startDate: '2019-01-03',
+    endDate: '2019-09-28',
+    titles: [
+      { title: 'Demon Slayer', language: 'x-jat', type: 'main' }
+    ],
+    description:
+    '*Ever since the death of his father, the burden of supporting the family has fallen upon Tanjirou Kamados shoulders. Though living impoverished on a remote mountain, the Kamado family are able to enjoy a relatively peaceful and happy life. One day, Tanjirou decides to go down to the local village to make a little money selling charcoal. On his way back, night falls, forcing Tanjirou to take shelter in the house of a strange man, who warns him of the existence of flesh-eating demons that lurk in the woods at night. When he finally arrives back home the next day, he is met with a horrifying sight—his whole family has been slaughtered. Worse still, the sole survivor is his sister Nezuko, who has been turned into a bloodthirsty demon. Consumed by rage and hatred, Tanjirou swears to avenge his family and stay by his only remaining sibling. Alongside the mysterious group calling themselves the Demon Slayer Corps, Tanjirou will do whatever it takes to slay the demons and protect the remnants of his beloved sisters humanity.',
 
-
-<anime id="1" restricted="false">
-<type>anime</type>
-   <episodecount>13</episodecount>
-   <titles>
-      <title xml:lang="x-jat" type="main">Fruits Basket</title>
-   </titles>
-   <recommendations total="1">
-      <recommendation type="Must See" uid="684759">Fruits Basket tells the story of Tohru Honda, an orphan girl who, after meeting Yuki, Kyo, and Shigure Sohma, learns that 13 members of the Sohma family are possessed by the animals of the Chinese zodiac and are cursed to turn into their animal forms when they are weak, stressed, or when they are embraced by anyone of the opposite sex who is not possessed by a spirit of the zodiac. As the series progresses, Tohru learns of the hardships and pain faced by the afflicted members of the Sohma family, and through her own generous and loving nature, helps heal their emotional wounds. As she learns more about Yuki, Kyo, and the rest of the mysterious Sohma family, Tohru also learns more about herself and how much others care for her.</recommendation>
-   </recommendations>
-    <genre>
-        Slice of life
-    </genre>
-   <ratings>
-      <permanent count="274">9.01</permanent>
-   </ratings>
-   <manga>Yes</manga>
-
-<anime id="2" restricted="false">
-<type>anime</type>
-<episodecount>27</episodecount>
-<titles>
-    <title xml:lang="x-jat" type="main">Demon Slayer</title>
-</titles>
-<recommendations total="1">
-    <recommendation type="Must See" uid="684759">Ever since the death of his father, the burden of supporting the family has fallen upon Tanjirou Kamado's shoulders. Though living impoverished on a remote mountain, the Kamado family are able to enjoy a relatively peaceful and happy life. One day, Tanjirou decides to go down to the local village to make a little money selling charcoal. On his way back, night falls, forcing Tanjirou to take shelter in the house of a strange man, who warns him of the existence of flesh-eating demons that lurk in the woods at night.
-
-    When he finally arrives back home the next day, he is met with a horrifying sight—his whole family has been slaughtered. Worse still, the sole survivor is his sister Nezuko, who has been turned into a bloodthirsty demon. Consumed by rage and hatred, Tanjirou swears to avenge his family and stay by his only remaining sibling. Alongside the mysterious group calling themselves the Demon Slayer Corps, Tanjirou will do whatever it takes to slay the demons and protect the remnants of his beloved sister's humanity.</recommendation>
-</recommendations>
-<genre>
-    Shonen
-</genre>
-<ratings>
-    <permanent count="274">8.52</permanent>
-</ratings>
-<manga>Yes</manga>
-
-<anime id="3" restricted="false">
-<type>anime</type>
-<episodecount>26</episodecount>
-<titles>
-    <title xml:lang="x-jat" type="main">Spy family</title>
-</titles>
-<recommendations total="1">
-    <recommendation type="Must See" uid="684759">Corrupt politicians, frenzied nationalists, and other warmongering forces constantly jeopardize the thin veneer of peace between neighboring countries Ostania and Westalis. In spite of their plots, renowned spy and master of disguise "Twilight" fulfills dangerous missions one after another in the hope that no child will have to experience the horrors of war.
-
-    In the bustling Ostanian city of Berlint, Twilight dons the alias of "Loid Forger," an esteemed psychiatrist. However, his true intention is to gather intelligence on prominent politician Donovan Desmond, who only appears rarely in public at his sons' school: the prestigious Eden Academy. Enlisting the help of unmarried city hall clerk Yor Briar to act as his wife and adopting the curious six-year-old orphan Anya as his daughter, Loid enacts his master plan. He will enroll Anya in Eden Academy, where Loid hopes she will excel and give him the opportunity to meet Donovan without arousing suspicion.
-
-    Unfortunately for Loid, even a man of his talents has trouble playing the figure of a loving father and husband. And just like Loid is hiding his true identity, Yor—who is an underground assassin known as "Thorn Princess"—and Anya—an esper who can read people's minds—have no plans to disclose their own secrets either. Although this picture-perfect family is founded on deception, the Forgers gradually come to understand that the love they share for one another trumps all else..</recommendation>
-</recommendations>
-<genre>
-    Slice of life
-</genre>
-<ratings>
-    <permanent count="274">8.69</permanent>
-</ratings>
-<manga>Yes</manga>
-
-<anime id="4" restricted="false">
-<type>anime</type>
-<episodecount>148</episodecount>
-<titles>
-    <title xml:lang="x-jat" type="main">Hunter x Hunter</title>
-</titles>
-<recommendations total="1">
-    <recommendation type="Must See" uid="684759">Hunters devote themselves to accomplishing hazardous tasks, all from traversing the world's uncharted territories to locating rare items and monsters. Before becoming a Hunter, one must pass the Hunter Examination—a high-risk selection process in which most applicants end up handicapped or worse, deceased.
-
-    Ambitious participants who challenge the notorious exam carry their own reason. What drives 12-year-old Gon Freecss is finding Ging, his father and a Hunter himself. Believing that he will meet his father by becoming a Hunter, Gon takes the first step to walk the same path.
-
-    During the Hunter Examination, Gon befriends the medical student Leorio Paladiknight, the vindictive Kurapika, and ex-assassin Killua Zoldyck. While their motives vastly differ from each other, they band together for a common goal and begin to venture into a perilous world.</recommendation>
-</recommendations>
-<genre>
-    Shonen
-</genre>
-<ratings>
-    <permanent count="274">8.52</permanent>
-</ratings>
-<manga>Yes</manga>
-
-<show id="1" restricted="false">
-<type>TV show</type>
-<episodecount>201</episodecount>
-<titles>
-    <title xml:lang="x-jat" type="main">The Office </title>
-</titles>
-<recommendations total="1">
-    <recommendation type="Must See" uid="684759">The Office is an American mockumentary sitcom television series that depicts the everyday work lives of office employees at the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company.</recommendation>
-</recommendations>
-<genre>
-    Comedy
-</genre>
-<ratings>
-    <permanent count="274">9.6</permanent>
-</ratings>
-
-<show id="2" restricted="false">
-<type>TV show</type>
-<episodecount>8</episodecount>
-<titles>
-    <title xml:lang="x-jat" type="main">Wednesday</title>
-</titles>
-<recommendations total="1">
-    <recommendation type="Must See" uid="684759">While attending Nevermore Academy, Wednesday Addams attempts to master her emerging psychic ability, thwart a killing spree and solve the mystery that embroiled her parents 25 years ago.</recommendation>
-</recommendations>
-<genre>
-    Comedy
-</genre>
-<ratings>
-    <permanent count="274">9.6</permanent>
-</ratings>
-
-<show id="3" restricted="false">
-<type>TV show</type>
-<episodecount>16</episodecount>
-<titles>
-    <title xml:lang="x-jat" type="main">The Witcher</title>
-</titles>
-<recommendations total="1">
-    <recommendation type="Must See" uid="684759">The witcher Geralt, a mutated monster hunter, struggles to find his place in a world in which people often prove more wicked than beasts.</recommendation>
-</recommendations>
-<genre>
-    Sci-fi
-</genre>
-<ratings>
-    <permanent count="274">8.1</permanent>
-</ratings>
+    ratings: {
+        permanent: { score: '8.52' },
+      }
+    }
