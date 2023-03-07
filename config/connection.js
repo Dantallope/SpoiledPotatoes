e2356c0cefbc88699030db00d7d2b89b7ec6877d
Dantallope <dantebraccialini@gmail.com>
2023-03-06 20:36:31 -0700

    troublshooting

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
-/* This will initialize sequelize and use the data put in the .env file, so if the code is working for others but not for you, you may need to fill out your .env file ;) */ 
+/* This will initialize sequelize and use the data put in the .env file, so if the code is working for others but not for you, you may need to fill out your .env file ;) */
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: 3306
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+      port: process.env.PORT
     }
   );
 
